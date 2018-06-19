a8d85e0c54ad679dfe23d1efd96f5b7a2d884f98
argonavt11 <argonavt.al@yandex.ru>
2018-06-19 21:19:56 +0300

    feat: array colors

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function roundImage(img = "https://cdn.wallpapersafari.com/26/87/4fsbSv.jpg") {
-  console.log(img);
+function roundImage(img) {
+  var canvas = document.getElementById("ex1");
+  const data = [];
+  if (canvas){
+    var context = canvas.getContext('2d');
+    var imageObj = new Image();
+    imageObj.onload = function() {
+      context.drawImage(imageObj, 0, 0);
+    };
+    imageObj.src = 'http://www.sboutdoors.com/elements/img/elements/logo-150.png.';
+    const imgData = context.getImageData(0, 0, 100, 100);
+    for (var i=0;i<imgData.data.length;i+=4)
+    {
+      data.push(`${255-imgData.data[i]}, ${255-imgData.data[i+1]}, ${255-imgData.data[i+2]}, 1`);
+    }
+    console.log(data);
+  }
 }
-
-roundImage();
