79b4b21e13f546eb824c644eedfeff0e144126f4
argonavt11 <argonavt.al@yandex.ru>
2018-06-21 01:21:53 +0300

    feat: used canvas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import RoundImage from 'components/round-image';
-import img from "./96387_1280_1024.jpg"
+import img from "./DfhZxS.jpg"
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      color: null
+    }
+  }
   componentDidMount(){
-    if(this.img) RoundImage.init(this.img)
+    if(this.img) {
+      const n = RoundImage.init(this.img, (color) => this.setState({color}));
+    }
   }
   render() {
     return (
-      <div className="App">
+      <div className="App" style={this.state.color ? {background: `rgb(${this.state.color})`} : {}}>
         <img
+          width={200}
           src={img}
           ref={(img) => this.img = img}
         />
